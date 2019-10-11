be484b56785dcde9f2b93b7476bf9a77569d082a
chenjianwy225 <chenjianwy225@sina.com>
2019-10-11 16:26:00 +0800

    2019-10-11提交

--- maven-web/src/test/java/com/test/Ts.java
+++ maven-web/src/test/java/com/test/Ts.java
@@ -1,19 +1,13 @@
 package com.test;
 
-import com.alibaba.fastjson.JSONObject;
-import com.maven.common.xml.XMLUtils;
+import com.maven.common.picture.PictureUtils;
 
 public class Ts {
 
 	public static void main(String[] args) throws Exception {
-		String filePath = "F:\\1.xml";
+		String picPath = "E:\\images\\1.jpg";
 
-		JSONObject jsonObject = XMLUtils.read(filePath);
-		System.out.println(jsonObject);
-
-		XMLUtils.write("F:\\2.xml", jsonObject);
-
-		byte[] byt = XMLUtils.write(jsonObject);
-		System.out.println(byt.length);
+		PictureUtils.zoomToTarget(picPath, "F:\\images", "1.png", 0.5);
+		PictureUtils.zoomToTarget(picPath, "F:\\images", "2.png", 800, 600);
 	}
 }
